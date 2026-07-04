239e9c0bcba6742d992ae016d548e425097cb849
hakimi <vtnhqw@gmail.com>
2026-07-04 08:01:11 +0800

    push

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const CONFIG = {
   SUPABASE_URL: "https://exzusyenhvqkfdqxedhy.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4enVzeWVuaHZxa2ZkcXhlZGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwODM1NDgsImV4cCI6MjA5ODY1OTU0OH0.7T3ADo6Fvv86dz3cB_evoycjw0yXHa_usQHh-1MPIsw",
   EMAILJS_SERVICE_ID: "service_jtr48xq",
-  EMAILJS_TEMPLATE_ID: "template_a4z9ndj",
+  EMAILJS_TEMPLATE_ID: "template_0mdiq5l",
   EMAILJS_PUBLIC_KEY: "W5Bed69S5LPNEN2ZC"
 };
